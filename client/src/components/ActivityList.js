3a24237f49fa862950f430b3aec0301e29790e97
bigpug <101833648+BigPugLabs@users.noreply.github.com>
2022-10-26 16:18:04 +0100

    fixed unhandled promise in ActivityList

--- client/src/components/ActivityList.js
+++ client/src/components/ActivityList.js
@@ -1,37 +1,25 @@
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
-const ActivityList = async ({ className }) => {
+const ActivityList = ({ className }) => {
 
     const [activities, setActivities] = useState([])
-
-    // if (!activities.length) {
-    //     try {
-    //         const res = await fetch("http://192.168.0.11:3003/api/activitylist", {credentials:'include'})
-    //         const data = await res.json()
-    //         setActivities(data)
-    //     } catch (err) {
-    //         console.log(err)
-    //     }
-    // }
 
     useEffect(() => {
         async function fetchActivityList() {
             try {
-                const res = await fetch("http://192.168.0.11:3003/api/activitylist", { credentials: 'include' })
+                const res = await fetch("/api/activitylist", { credentials: 'include' })
                 const data = await res.json()
-                console.log(data)
+                //console.log(data)
                 setActivities(data)
             } catch (err) {
                 console.log(err)
             }
         }
         fetchActivityList()
-        // fetch("http://192.168.0.11:3003/api/activitylist", { credentials: 'include' })
-        //     .then(res => res.json())
-        //     .then(data => setActivities(data))
-        //     .catch(err => console.log(err))
     }, [])
+
+    const list = activities.map(el=><li key={el._id}>{el.name}</li>)
 
     return (
         <ul className={className}>
@@ -40,6 +28,7 @@ const ActivityList = async ({ className }) => {
                 <span>19/07/2022, 21:20:04</span>
             </li>
             <li>{activities.length}</li>
+            {list}
         </ul>
     )
 }
